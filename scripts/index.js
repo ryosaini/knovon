565abecf4794e9158a5750cc23eaf0d88f5867f3
Rahulsaini96 <rahul.saini@knovonconsulting.com>
2023-02-24 20:33:15 +0530

    final v1

---
+++ scripts/index.js
@@ -0,0 +1,81 @@
+
+
+    $(document).ready(function () {
+      var owllogo = $('.owl-carousel_logo');
+      var owlCulture = $('.owl-carousel_Culture');
+      var owl = $('.owl-carousel_Case');
+      var owlbanner = $('.owl-carousel_banner');
+      owllogo.owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 4,
+        autoplay: true,
+        slideSpeed: 800,
+        autoPlay: 6000,
+        smartSpeed: 2000,
+        responsive: {
+          0: {
+            items: 2
+          },
+          600: {
+            items: 4
+          }
+        }
+      });
+      owlCulture.owlCarousel({
+        center: false,
+        items: 4,
+        loop: true,
+        autoplay: true,
+        slideSpeed: 800,
+        autoPlay: 10000,
+        smartSpeed: 2000,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 4
+          }
+        }
+      });
+      owl.owlCarousel({
+        autoplayHoverPause:true,
+        items: 1,
+        autoplay: false,
+        loop: true,
+        slideSpeed: 800,
+        autoPlay: 10000,
+        smartSpeed: 2000,
+
+      });
+      owlbanner.owlCarousel({
+        stagePadding: 0,
+        margin: 0,
+        items: 1,
+        loop: true,
+        autoplay: true,
+        slideSpeed: 1000,
+        smartSpeed: 2000,
+        nav: true,
+        navText: [" ", "<img src='assets/img/next-btn.png' class='next-btn'>"],
+
+      });
+      // owl.on("mousewheel", ".owl-stage", function (e) {
+      //     if (e.originalEvent.wheelDelta > 0) {
+      //       owl.trigger("next.owl");
+      //     } else {
+      //       owl.trigger("prev.owl");
+      //     }
+      //     e.preventDefault();
+      //   });
+      AOS.init({});
+    });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+          $('nav').addClass('shade');
+        } else {
+          $("nav").removeClass("shade");
+  
+        }
+      });
